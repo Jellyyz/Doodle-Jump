0690dbc2f21425572999a9c6a3fcb57cb57d8900
BuildTools <unconfigured@null.spigotmc.org>
2022-04-15 23:48:23 -0500

    first commit

---
+++ software/DoodleJump/obj/default/main.d
@@ -0,0 +1,45 @@
+obj/default/main.o: main.c ../DoodleJump_bsp/system.h \
+ ../DoodleJump_bsp/linker.h \
+ ../DoodleJump_bsp//drivers/inc/altera_avalon_spi.h \
+ ../DoodleJump_bsp//HAL/inc/alt_types.h \
+ ../DoodleJump_bsp//drivers/inc/altera_avalon_spi_regs.h \
+ ../DoodleJump_bsp//HAL/inc/io.h ../DoodleJump_bsp//HAL/inc/alt_types.h \
+ ../DoodleJump_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../DoodleJump_bsp//HAL/inc/sys/alt_irq.h \
+ ../DoodleJump_bsp//HAL/inc/nios2.h usb_kb/GenericMacros.h \
+ usb_kb/GenericTypeDefs.h usb_kb/HID.h usb_kb/MAX3421E.h \
+ usb_kb/transfer.h usb_kb/usb_ch9.h usb_kb/USB.h
+
+../DoodleJump_bsp/system.h:
+
+../DoodleJump_bsp/linker.h:
+
+../DoodleJump_bsp//drivers/inc/altera_avalon_spi.h:
+
+../DoodleJump_bsp//HAL/inc/alt_types.h:
+
+../DoodleJump_bsp//drivers/inc/altera_avalon_spi_regs.h:
+
+../DoodleJump_bsp//HAL/inc/io.h:
+
+../DoodleJump_bsp//HAL/inc/alt_types.h:
+
+../DoodleJump_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../DoodleJump_bsp//HAL/inc/sys/alt_irq.h:
+
+../DoodleJump_bsp//HAL/inc/nios2.h:
+
+usb_kb/GenericMacros.h:
+
+usb_kb/GenericTypeDefs.h:
+
+usb_kb/HID.h:
+
+usb_kb/MAX3421E.h:
+
+usb_kb/transfer.h:
+
+usb_kb/usb_ch9.h:
+
+usb_kb/USB.h:
