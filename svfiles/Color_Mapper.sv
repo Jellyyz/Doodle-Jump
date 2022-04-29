a76511edbfc752e869efb6a7148425a508f36375
Jellyyz <gallyhuang@gmail.com>
2022-04-28 22:54:59 -0500

    added random platforms fix/more easy game

--- svfiles/Color_Mapper.sv
+++ svfiles/Color_Mapper.sv
@@ -10,6 +10,13 @@
 //                                                                       --
 //    For use with ECE 385 Lab 7                                         --
 //    University of Illinois ECE Department                              --
+//    Modifed by Gally && Feng Zhao April/May 2022                      --
+//    University of Illinois ECE Department                              --
+//    University of Illinois ECE Department                              --
+//    University of Illinois ECE Department                              --
+//    University of Illinois ECE Department                              --
+//    University of Illinois ECE Department                              --
+//    University of Illinois ECE Department                              --
 //-------------------------------------------------------------------------
 
 
@@ -48,57 +55,80 @@ module  color_mapper (
     logic Doodle_on;
 
 
-  
 
+logic [8:0] seedgen;   
+logic [8:0] plat_ready_trigger; 
+counter seedgenx(
+	.Reset(0), 
+	.enable(1), 
+    .Clk(Clk), 
+
+    .out(seedgen[8:0])
+);
+counter platrdy(
+	.Reset(0), 
+	.enable(1), 
+    .Clk(frame_clk), 
+
+    .out(plat_ready_trigger[8:0])
+);
+
+always_comb 
+    begin 
+        if(Reset)
+            res_LFSR = Reset; 
+        else 
+            res_LFSR = 0; 
+    end
 // these LFSR are a chain of Shift Registers that generate 16 strings of 9 bit random numbers for platX
 logic seed_en, seed_en1, seed_en2, seed_en3, seed_en4, seed_en5, seed_en6, seed_en7, seed_en8, seed_en9, seed_en10, seed_en11, seed_en12, seed_en13, seed_en14, seed_en15;
 LFSR LFSR(
-    .Clk(Clk), .Reset(Reset), .outp(testX[8:0]), .seed_out(seed_en)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX[8:0]), .seed(seedgen[8:0]), .seed_in(seed_en15), .seed_out(seed_en)
 );
 LFSR LFSR1(
-    .Clk(Clk), .Reset(Reset), .outp(testX1[8:0]), .seed(testX[8:0]), .seed_in(seed_en), .seed_out(seed_en1)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX1[8:0]), .seed(testX[8:0]), .seed_in(seed_en), .seed_out(seed_en1)
 );
 LFSR LFSR2(
-    .Clk(Clk), .Reset(Reset), .outp(testX2[8:0]), .seed(testX1[8:0]), .seed_in(seed_en1), .seed_out(seed_en2)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX2[8:0]), .seed(testX1[8:0]), .seed_in(seed_en1), .seed_out(seed_en2)
 );
 LFSR LFSR3(
-    .Clk(Clk), .Reset(Reset), .outp(testX3[8:0]), .seed(testX2[8:0]), .seed_in(seed_en2), .seed_out(seed_en3)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX3[8:0]), .seed(testX2[8:0]), .seed_in(seed_en2), .seed_out(seed_en3)
 );
 LFSR LFSR4(
-    .Clk(Clk), .Reset(Reset), .outp(testX4[8:0]), .seed(testX3[8:0]), .seed_in(seed_en3), .seed_out(seed_en4)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX4[8:0]), .seed(testX3[8:0]), .seed_in(seed_en3), .seed_out(seed_en4)
 );
 LFSR LFSR5(
-    .Clk(Clk), .Reset(Reset), .outp(testX5[8:0]), .seed(testX4[8:0]), .seed_in(seed_en4), .seed_out(seed_en5)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX5[8:0]), .seed(testX4[8:0]), .seed_in(seed_en4), .seed_out(seed_en5)
 );
 LFSR LFSR6(
-    .Clk(Clk), .Reset(Reset), .outp(testX6[8:0]), .seed(testX5[8:0]), .seed_in(seed_en5), .seed_out(seed_en6)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX6[8:0]), .seed(testX5[8:0]), .seed_in(seed_en5), .seed_out(seed_en6)
 );
 LFSR LFSR7(
-    .Clk(Clk), .Reset(Reset), .outp(testX7[8:0]), .seed(testX6[8:0]), .seed_in(seed_en6), .seed_out(seed_en7)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX7[8:0]), .seed(testX6[8:0]), .seed_in(seed_en6), .seed_out(seed_en7)
 );
 LFSR LFSR8(
-    .Clk(Clk), .Reset(Reset), .outp(testX8[8:0]), .seed(testX7[8:0]), .seed_in(seed_en7), .seed_out(seed_en8)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX8[8:0]), .seed(testX7[8:0]), .seed_in(seed_en7), .seed_out(seed_en8)
 );
 LFSR LFSR9(
-    .Clk(Clk), .Reset(Reset), .outp(testX9[8:0]), .seed(testX8[8:0]), .seed_in(seed_en8), .seed_out(seed_en9)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX9[8:0]), .seed(testX8[8:0]), .seed_in(seed_en8), .seed_out(seed_en9)
 );
 LFSR LFSR10(
-    .Clk(Clk), .Reset(Reset), .outp(testX10[8:0]), .seed(testX9[8:0]), .seed_in(seed_en9), .seed_out(seed_en10)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX10[8:0]), .seed(testX9[8:0]), .seed_in(seed_en9), .seed_out(seed_en10)
 );
 LFSR LFSR11(
-    .Clk(Clk), .Reset(Reset), .outp(testX11[8:0]), .seed(testX10[8:0]), .seed_in(seed_en10), .seed_out(seed_en11)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX11[8:0]), .seed(testX10[8:0]), .seed_in(seed_en10), .seed_out(seed_en11)
 );
 LFSR LFSR12(
-    .Clk(Clk), .Reset(Reset), .outp(testX12[8:0]), .seed(testX11[8:0]), .seed_in(seed_en11), .seed_out(seed_en12)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX12[8:0]), .seed(testX11[8:0]), .seed_in(seed_en11), .seed_out(seed_en12)
 );
 LFSR LFSR13(
-    .Clk(Clk), .Reset(Reset), .outp(testX13[8:0]), .seed(testX12[8:0]), .seed_in(seed_en12), .seed_out(seed_en13)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX13[8:0]), .seed(testX12[8:0]), .seed_in(seed_en12), .seed_out(seed_en13)
 );
 LFSR LFSR14(
-    .Clk(Clk), .Reset(Reset), .outp(testX14[8:0]), .seed(testX13[8:0]), .seed_in(seed_en13), .seed_out(seed_en14)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX14[8:0]), .seed(testX13[8:0]), .seed_in(seed_en13), .seed_out(seed_en14)
 );
 LFSR LFSR15(
-    .Clk(Clk), .Reset(Reset), .outp(testX15[8:0]), .seed(testX15[8:0]), .seed_in(seed_en14), .seed_out(seed_en15)
+    .Clk(Clk), .Reset(res_LFSR), .outp(testX15[8:0]), .seed(testX14[8:0]), .seed_in(seed_en14), .seed_out(seed_en15)
 );
 logic [8:0] testX1, testX2, testX3, testX4, testX5, testX6, testX7,  testX8, testX9, testX10, testX11, testX12, testX13, testX14, testX15;
 logic [8:0] readyX1, readyX2, readyX3, readyX4, readyX5, readyX6, readyX7,  readyX8, readyX9, readyX10, readyX11, readyX12, readyX13, readyX14, readyX15;
@@ -126,113 +156,175 @@ always_comb
 		endcase 
     end 
 logic plat_ready; 
-
-always_ff @ (posedge Clk)
+// safety to ensure game is winnable
+always_ff @ (posedge frame_clk)
 begin 
-    if (testX > 9'd0 && testX[7:0] <= 8'd256) 
-        readyX  <= testX[7:0];
-    if (testX1 >= 9'd201 && testX1 <= 9'd400) 
+    if (testX <= 9'h1E0) 
+        readyX <= testX;
+    if (1) 
         readyX1 <= testX1;
-    if (testX2 > 9'd0 && testX2[7:0] <= 8'd256) 
-        readyX2 <= testX2[7:0];
-    if (testX3 >= 9'd201 && testX3 <= 9'd400) 
-        readyX3 <= testX3[7:0];
-    if (testX4 > 9'd0 && testX4[7:0] <= 8'd256)
-        readyX4 <= testX4[7:0];
-    if (testX5 >= 9'd201 && testX5 <= 9'd400)
+    if (testX2 <= 9'h1E0) 
+        readyX2 <= testX2;
+    if (1) 
+        readyX3 <= testX3;
+    if (testX4 <= 9'h1E0)
+        readyX4 <= testX4;
+    if (1)
         readyX5 <= testX5;
-    if (testX6 > 9'd0 && testX6[7:0] <= 8'd256)
-        readyX6 <= testX6[7:0];
-    if (testX7 >= 9'd201 && testX7 <= 9'd400) 
+    if (testX6 <= 9'h1E0)
+        readyX6 <= testX6;
+    if (1) 
         readyX7 <= testX7;
-    if (testX8 > 9'd0 && testX8[7:0] <= 8'd256)
-        readyX8 <= testX8[7:0];
-    if (testX9 >= 9'd201 && testX9 <= 9'd400)
-        readyX9 <= testX9[7:0];
-    if (testX10 > 9'd0 && testX10[7:0] <= 8'd256)
-        readyX10 <= testX10[7:0];
-    if (testX11 >= 9'd201 && testX11 <= 9'd400)
+    if (testX8 <= 9'h1E0)
+        readyX8 <= testX8;
+    if (1)
+        readyX9 <= testX9;
+    if (testX10 <= 9'h1E0)
+        readyX10 <= testX10;
+    if (1)
         readyX11 <= testX11;
-    if (testX12 > 9'd0 && testX12[7:0] <= 8'd256)
-        readyX12 <= testX12[7:0];
-    if (testX13 >= 9'd201 && testX13 <= 9'd400)
+    if (testX12 <= 9'h1E0)
+        readyX12 <= testX12;
+    if (1)
         readyX13 <= testX13;
-    if (testX14 > 9'd0 && testX14[7:0] <= 8'd256)
-        readyX14 <= testX14[7:0];
-    if (testX15 >= 9'd201 && testX15 <= 9'd400) 
+    if (testX14 <= 9'h1E0)
+        readyX14 <= testX14;
+    if (1) 
         readyX15 <= testX15; 
 end 
 // this is for calculation of platX
-always_ff @ (posedge loadplat)
-    begin 
-        if(loadplat)
-        begin 
-            if(readyX >= 9'h0 && readyX <= 9'd400)
-                platX = readyX + 9'd100;
-            else 
-                platX = readyX; 
-            if(readyX1 >= 9'h0 && readyX1 <= 9'd400)
-                platX1 = readyX1 + 9'd100;
-            else 
-                platX1 = readyX1; 
-            if(readyX2 >= 9'h0 && readyX2 <= 9'd400)
-                platX2 = readyX2 + 9'd100; 
-            else 
-                platX2 = readyX2; 
-            if(readyX3 >= 9'h0 && readyX3 <= 9'd400)
-                platX3 = readyX3 + 9'd100;  
-            else 
-                platX3 = readyX3; 
-            if(readyX4 >= 9'h0 && readyX4 <= 9'd400)
-                platX4 = readyX4 + 9'd100; 
-            else 
-                platX4 = readyX4; 
-            if(readyX5 >= 9'h0 && readyX5 <= 9'd400)
-                platX5 = readyX5 + 9'd100; 
-            else 
-                platX5 = readyX5; 
-            if(readyX6 >= 9'h0 && readyX6 <= 9'd400)
-                platX6 = readyX6 + 9'd100; 
-            else 
-                platX6 = readyX6; 
-            if(readyX7 >= 9'h0 && readyX7 <= 9'd400)
-                platX7 = readyX7 + 9'd100;
-            else 
-                platX7 = readyX7; 
-            if(readyX8 >= 9'h0 && readyX8 <= 9'd400)
-                platX8 = readyX8 + 9'd100; 
-            else 
-                platX8 = readyX8; 
-            if(readyX9 >= 9'h0 && readyX9 <= 9'd400)
-                platX9 = readyX9 + 9'd100; 
-            else 
-                platX9 = readyX9; 
-            if(readyX10 >= 9'h0 && readyX10 <= 9'd400)
-                platX10 = readyX10 + 9'd100; 
-            else 
-                platX10 = readyX10; 
-            if(readyX11 >= 9'h0 && readyX11 <= 9'd400)
-                platX11 = readyX11 + 9'd100;      
-            else 
-                platX11 = readyX11; 
-            if(readyX12 >= 9'h0 && readyX12 <= 9'd400)
-                platX12 = readyX12 + 9'd100; 
-            else 
-                platX12 = readyX12; 
-            if(readyX13 >= 9'h0 && readyX13 <= 9'd400)
-                platX13 = readyX13 + 9'd100;
-            else 
-                platX13 = readyX13; 
-            if(readyX14 >= 9'h0 && readyX14 <= 9'd400)
-                platX14 = readyX14 + 9'd100; 
-            else 
-                platX14 = readyX14; 
-            if(readyX15 >= 9'h0 && readyX15 <= 9'd400)
-                platX15 = readyX15 + 9'd100;    
-            else 
-                platX15 = readyX15; 
-        end
+always_ff @ (posedge loadplat or 
+posedge plat_offscreen[0] or
+posedge plat_offscreen[1] or
+posedge plat_offscreen[2] or
+posedge plat_offscreen[3] or
+posedge plat_offscreen[4] or
+posedge plat_offscreen[5] or
+posedge plat_offscreen[6] or
+posedge plat_offscreen[7] or
+posedge plat_offscreen[8] or
+posedge plat_offscreen[9] or
+posedge plat_offscreen[10] or
+posedge plat_offscreen[11] or
+posedge plat_offscreen[12] or
+posedge plat_offscreen[13] or
+posedge plat_offscreen[14] or
+posedge plat_offscreen[15]
 
+)
+    begin 
+        if(loadplat || plat_offscreen[0])
+            begin 
+                if(readyX > 9'h0 && readyX <= 9'd100)
+                    platX = readyX + 9'd100;
+                else 
+                    platX = readyX; 
+            end 
+        if(loadplat || plat_offscreen[1])
+            begin 
+                if(readyX1 > 9'h0 && readyX1 <= 9'd100)
+                    platX1 = readyX1 + 9'd100;
+                else 
+                    platX1 = readyX1; 
+            end 
+        if(loadplat || plat_offscreen[2])
+            begin 
+                if(readyX2 > 9'h0 && readyX2 <= 9'd100)
+                    platX2 = readyX2 + 9'd100;
+                else 
+                    platX2 = readyX2; 
+            end 
+        if(loadplat || plat_offscreen[3])
+            begin 
+                if(readyX3 > 9'h0 && readyX3 <= 9'd100)
+                    platX3 = readyX3 + 9'd100;
+                else 
+                    platX3 = readyX3; 
+            end 
+        if(loadplat || plat_offscreen[4])
+            begin 
+                if(readyX4 > 9'h0 && readyX4 <= 9'd100)
+                    platX4 = readyX4 + 9'd100;
+                else 
+                    platX4 = readyX4; 
+            end 
+        if(loadplat || plat_offscreen[5])
+            begin 
+                if(readyX5 > 9'h0 && readyX5 <= 9'd100)
+                    platX5 = readyX5 + 9'd100;
+                else 
+                    platX5 = readyX5; 
+            end 
+        if(loadplat || plat_offscreen[6])
+            begin 
+                if(readyX6 > 9'h0 && readyX6 <= 9'd100)
+                    platX6 = readyX6 + 9'd100;
+                else 
+                    platX6 = readyX6; 
+            end 
+        if(loadplat || plat_offscreen[7])
+            begin 
+                if(readyX7 > 9'h0 && readyX7 <= 9'd100)
+                    platX7 = readyX7 + 9'd100;
+                else 
+                    platX7 = readyX7; 
+            end
+        if(loadplat || plat_offscreen[8])
+            begin 
+                if(readyX8 > 9'h0 && readyX8 <= 9'd100)
+                    platX8 = readyX8 + 9'd100;
+                else 
+                    platX8 = readyX8; 
+            end 
+        if(loadplat || plat_offscreen[9])
+            begin 
+                if(readyX9 > 9'h0 && readyX9 <= 9'd100)
+                    platX9 = readyX9 + 9'd100;
+                else 
+                    platX9 = readyX9; 
+            end 
+        if(loadplat || plat_offscreen[10])
+            begin 
+                if(readyX10 > 9'h0 && readyX10 <= 9'd100)
+                    platX10 = readyX10 + 9'd100;
+                else 
+                    platX10 = readyX10; 
+            end 
+        if(loadplat || plat_offscreen[11])
+            begin 
+                if(readyX11 > 9'h0 && readyX11 <= 9'd100)
+                    platX11 = readyX11 + 9'd100;
+                else 
+                    platX11 = readyX11; 
+            end 
+        if(loadplat || plat_offscreen[12])
+            begin 
+                if(readyX12 > 9'h0 && readyX12 <= 9'd100)
+                    platX12 = readyX12 + 9'd100;
+                else 
+                    platX12 = readyX12; 
+            end 
+        if(loadplat || plat_offscreen[13])
+            begin 
+                if(readyX13 > 9'h0 && readyX13 <= 9'd100)
+                    platX13 = readyX13 + 9'd100;
+                else 
+                    platX13 = readyX13; 
+            end 
+        if(loadplat || plat_offscreen[14])
+            begin 
+                if(readyX14 > 9'h0 && readyX14 <= 9'd100)
+                    platX14 = readyX14 + 9'd100;
+                else 
+                    platX14 = readyX14; 
+            end 
+        if(loadplat || plat_offscreen[15])
+            begin 
+                if(readyX15 > 9'h0 && readyX15 <= 9'd100)
+                    platX15 = readyX15 + 9'd100;
+                else 
+                    platX15 = readyX15; 
+            end  
     end 
 counter counterplat(
 	.Reset(plat_reset), 
@@ -250,7 +342,77 @@ countdown countdown(
     .temp(temp),
     .done(stop_refresh_flag)
 );
+logic [15:0] plat_offscreen; 
 
+always_ff @ (posedge Clk)
+    begin 
+        // tracks if a platforms has exited the bottom of the screen
+        if(platY - plat_sizeY > 9'd480)
+            plat_offscreen[0] = 1;
+        else   
+            plat_offscreen[0] = 0;  
+        if(platY1 - plat_sizeY > 9'd480)
+            plat_offscreen[1] = 1;
+        else   
+            plat_offscreen[1] = 0;  
+        if(platY2 - plat_sizeY > 9'd480)
+            plat_offscreen[2] = 1;
+        else   
+            plat_offscreen[2] = 0;  
+        if(platY3 - plat_sizeY > 9'd480)
+            plat_offscreen[3] = 1;
+        else   
+            plat_offscreen[3] = 0;  
+        if(platY4 - plat_sizeY > 9'd480)
+            plat_offscreen[4] = 1;
+        else   
+            plat_offscreen[4] = 0;  
+        if(platY5 - plat_sizeY > 9'd480)
+            plat_offscreen[5] = 1;
+        else   
+            plat_offscreen[5] = 0;  
+        if(platY6 - plat_sizeY > 9'd480)
+            plat_offscreen[6] = 1;
+        else   
+            plat_offscreen[6] = 0;  
+        if(platY7 - plat_sizeY > 9'd480)
+            plat_offscreen[7] = 1;
+        else   
+            plat_offscreen[7] = 0;  
+        if(platY8 - plat_sizeY > 9'd480)
+            plat_offscreen[8] = 1;
+        else   
+            plat_offscreen[8] = 0;  
+        if(platY9 - plat_sizeY > 9'd480)
+            plat_offscreen[9] = 1;
+        else   
+            plat_offscreen[9] = 0;  
+        if(platY10 - plat_sizeY > 9'd480)
+            plat_offscreen[10] = 1;
+        else   
+            plat_offscreen[10] = 0;  
+        if(platY11 - plat_sizeY > 9'd480)
+            plat_offscreen[11] = 1;
+        else   
+            plat_offscreen[11] = 0;  
+        if(platY12 - plat_sizeY > 9'd480)
+            plat_offscreen[12] = 1;
+        else   
+            plat_offscreen[12] = 0;  
+        if(platY13 - plat_sizeY > 9'd480)
+            plat_offscreen[13] = 1;
+        else   
+            plat_offscreen[13] = 0;  
+        if(platY14 - plat_sizeY > 9'd480)
+            plat_offscreen[14] = 1;
+        else   
+            plat_offscreen[14] = 0;  
+        if(platY15 - plat_sizeY > 9'd480)
+            plat_offscreen[15] = 1;
+        else   
+            plat_offscreen[15] = 0;  
+        
+    end 
 always_ff @ (posedge frame_clk or posedge loadplat)
     begin 
         // initial Y values of the platforms before anything
